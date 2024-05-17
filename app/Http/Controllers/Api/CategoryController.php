bc14c05e886cdb989015f5011e86b14688fc2a97
Eduardo Dahmer <eduardodahmer99@gmail.com>
2024-05-17 14:51:48 -0300

    projeto do curso de apisRestFull com laravel da especializaTI

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateCategoryFormRequest;
+
+class CategoryController extends Controller
+{
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    public function index(Category $category, Request $request)
+    {
+        $categories = $category->getResults($request->name);
+
+        return response()->json($categories, 200);
+    }
+
+    public function show($id)
+    {
+        if(!$category = $this->category->find($id)) {
+            return response()->json(['error' => 'Not Found!'], 404);
+        }
+        return response()->json($category);
+    }
+
+    public function store(StoreUpdateCategoryFormRequest $request)
+    {
+        $category = $this->category->create($request->all());
+        return response()->json($category, 201);
+    }
+
+    public function update(StoreUpdateCategoryFormRequest $request, $id)
+    {
+        if(!$category = $this->category->find($id)) {
+            return response()->json(['error' => 'Not Found!'], 404);
+        }
+
+        $category->update($request->all());
+
+        return response()->json($category);
+    }
+
+    public function destroy($id)
+    {
+        if(!$category = $this->category->find($id)) {
+            return response()->json(['error' => 'Not Found!'], 404);
+        }
+
+        $category->delete();
+
+        return response()->json(['message' => 'Registro deletado com sucesso']);
+    }
+}
